9aaa8698eafc7dad99d28149603f87254765a39d
KaparapuBharath <kaparapu1bharath@gmail.com>
2026-03-12 02:33:27 +0530

    fix chat api path

--- frontend/src/services/chatService.js
+++ frontend/src/services/chatService.js
@@ -1,11 +1,11 @@
 import API from "./api";
 
 export const sendMessage = async (message) => {
-  const res = await API.post("/chat", { message });
+  const res = await API.post("/api/chat", { message });
   return res.data;
 };
 
 export const getConversations = async () => {
-  const res = await API.get("/chat");
+  const res = await API.get("/api/chat");
   return res.data;
 };
