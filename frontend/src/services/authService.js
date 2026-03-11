661a31760073e6aa9f88b10822c6a7bb6f42d8c9
KaparapuBharath <kaparapu1bharath@gmail.com>
2026-03-12 02:20:22 +0530

    set API url

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,12 +1,12 @@
 import API from "./api";
 
 const register = async (data) => {
-  const res = await API.post("/auth/register", data);
+  const res = await API.post("/api/auth/register", data);
   return res.data;
 };
 
 const login = async (data) => {
-  const res = await API.post("/auth/login", data);
+  const res = await API.post("/api/auth/login", data);
   return res.data;
 };
 
